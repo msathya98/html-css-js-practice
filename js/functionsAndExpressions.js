c015de52ecc0d389d23a562c108a85c3fa831d58
Sathya M <62427113+msathya98@users.noreply.github.com>
2021-03-04 20:00:25 +0530

    simple example for function declration and function expression.

---
+++ js/functionsAndExpressions.js
@@ -0,0 +1,18 @@
+//functions Declaration
+
+function add(a, b) {
+
+   console.log(a+b);
+   //var a, b;
+}
+add(1, 3); 
+
+
+// Function Expression
+
+var foo = function add(a, b){
+    console.log(a+b);
+
+}
+    
+foo(3, 3);
