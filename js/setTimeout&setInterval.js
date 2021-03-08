5f381796d519780450cd4f4bfbec6bfc7f8a32ae
Sathya M <62427113+msathya98@users.noreply.github.com>
2021-03-08 22:59:57 +0530

    setTimeout and setInterval function with simple example.

---
+++ js/setTimeout&setInterval.js
@@ -0,0 +1,21 @@
+
+//setTimeout() function
+
+setTimeout(timer, 2000);
+
+function timer() {
+    console.log("sathya");
+}
+
+
+
+//setInterval() function 
+
+setInterval(timer2, 1000);
+
+function timer2() {
+    console.log("Welcome");
+}
+
+
+
