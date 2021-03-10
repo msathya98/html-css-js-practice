5105f79b55d40811a2255d558a3819cbd6ef79d2
Sathya M <62427113+msathya98@users.noreply.github.com>
2021-03-10 11:13:25 +0530

    class&Id.js with class and id priorities updated.

---
+++ CSS/class&Id.js
@@ -0,0 +1,15 @@
+<div class="section">
+     <p class="para" id="test">It should be dark</p>
+ </div>
+<style>
+    
+        .section .para  {
+            color: red;
+            background-color: red;
+        }
+        #test{
+            color: green;
+        }
+
+</style>
+ 
