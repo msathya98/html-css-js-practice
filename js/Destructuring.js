3f24cd9977fd17823200169b4cc6d351e7ddf1c5
Sathya M <62427113+msathya98@users.noreply.github.com>
2021-03-15 19:55:48 +0530

    Object destructuring within a array updated.

---
+++ js/Destructuring.js
@@ -0,0 +1,9 @@
+const comp = [{ name: "google", loc: "banglore"},
+{ name: "FB", loc: "Mysore"},
+{ name: "HP", loc: "Mandya"}]
+
+const [ { }, { loc: l1}, { loc: l2} ] = comp;
+console.log(l1, l2);
+console.log(l2);
+
+
